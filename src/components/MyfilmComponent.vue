99ba0ea72f68146568155929d3b433e009180103
Guillaume Carrio <guillaume.carrio.84@gmail.com>
2024-04-19 14:39:40 +0200

    👌 IMPROVE: css is ok 🎵

--- src/components/MyfilmComponent.vue
+++ src/components/MyfilmComponent.vue
@@ -16,11 +16,30 @@
             let i = myfilms.indexOf(film)
             myfilms.splice(i,1)
         }
-        localStorage.setItem('myFilms',JSON.stringify(myfilms))
+        localStorage.setItem('myFilms',JSON.stringify(myfilms));
+        location.reload();
     }
 </script>
 
 <style scoped>
+    button {
+        border: solid 2px black;
+        margin: 2vh;
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        margin: 1%;
+        padding: 2vh;
+        width: 22vw;
+        background-color: lightgray;
+    }
+    div:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
     img {
         width: 20vw;
         height: 30vh;
