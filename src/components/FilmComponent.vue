e07d12f0f28e9ffe870ca536bd63e16c9257365f
Guillaume Carrio <guillaume.carrio.84@gmail.com>
2024-04-19 11:13:40 +0200

    👌 IMPROVE: Add myfilms list

--- src/components/FilmComponent.vue
+++ src/components/FilmComponent.vue
@@ -4,11 +4,19 @@
         <p>
             {{ film.Title }} : Année = {{ film.Year }}
         </p>
+        <button @click="ajout(film.id)">Ajouter ce film a mon compte</button>
     </div>
 </template>
 
 <script setup>
     const film = defineModel('film')
+    const ajout = (film) => {
+        let myfilms = JSON.parse(localStorage.getItem('myFilms'))
+        if (!myfilms.includes(film)){
+            myfilms.push(film)
+        }
+        localStorage.setItem('myFilms',JSON.stringify(myfilms));
+    }
 </script>
 
 <style scoped>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,8 +1,30 @@
 <template>
     <div>
-        
+        <h1>
+            Liste des films
+        </h1>
+        <FilmComponent v-for="(film, ind) in films" v-model:film="films[ind]"/>
     </div>
 </template>
-<script>
-    
+<script setup>
+    import { reactive, onMounted } from "vue";
+
+    import axios from 'axios';
+    import FilmComponent from '../components/FilmComponent.vue';
+
+    let films = reactive([]);
+    const filmsid = JSON.parse(localStorage.getItem('myFilms'));
+    onMounted(() => {
+        axios
+            .get(`http://localhost:5555/films`)
+            .then((f) => {
+                f.data.forEach(elt => {
+                    if(filmsid.includes(elt.id)){
+                        films.push(elt)
+                    }
+                })
+            })
+            .catch(() => erreur = 'Pb de récup des films')
+    })
+
 </script>
